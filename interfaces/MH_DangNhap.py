5063410af2d5c1722be8c44d3171d532daea89ab
FoxSys285 <ntn200006@gmail.com>
2025-11-21 22:04:21 +0700

    Add files via upload
    Thêm page trang chủ, sửa lại các frame

---
+++ interfaces/MH_DangNhap.py
@@ -0,0 +1,114 @@
+from objects.TaiKhoan import TaiKhoan, DanhSachTaiKhoan
+
+import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk
+from datetime import datetime
+
+import os
+
+# MÀN HÌNH ĐĂNG NHẬP
+class MH_DangNhap(tk.Frame):
+    def __init__(self, parent, controller):
+
+        dulieu_path = "data/du_lieu_tk.json"
+
+        self.tb_text = tk.StringVar(value = "") # Thông báo đăng nhập
+
+        list_user = DanhSachTaiKhoan()
+        list_user.doc_file(dulieu_path)
+
+        for i in list_user.ds:
+            print(i)
+
+        super().__init__(parent, bg="#f9f4ef")
+        self.controller = controller
+
+        full_frame = tk.Frame(self, bg="#f9f4ef")
+        full_frame.place(x=0, y=0, relwidth=1, relheight=1)
+
+        # ===================== TIÊU ĐỀ =====================
+        hien_thi = tk.Frame(self, bg="#f9f4ef")
+        hien_thi.place(x=470, y=340)
+
+        tk.Label(
+            hien_thi, text="Đăng nhập - Giao ca",
+            fg="#990000", bg="#f9f4ef",
+            font=("proxima-nova", 25, "bold")
+        ).pack(fill="both", expand=True)
+
+        # ===================== KHUNG ĐĂNG NHẬP =====================
+        dang_nhap_frame = tk.Frame(self, bg="#f9f4ef")
+        dang_nhap_frame.place(x=470, y= 400, width=300, height=60)
+
+        tk.Label(dang_nhap_frame, text="Username",
+                 bg="#f9f4ef", fg="#003300",
+                 font=("proxima-nova", 12, "bold")).place(x=0, y=0, width=100)
+        username_entry = tk.Entry(dang_nhap_frame, width=30,
+                                  font=("proxima-nova", 13))
+        username_entry.place(x=100, y=0)
+
+        tk.Label(dang_nhap_frame, text="Password",
+                 bg="#f9f4ef", fg="#003300",
+                 font=("proxima-nova", 12, "bold")).place(x=0, y=30, width=100)
+        password_entry = tk.Entry(dang_nhap_frame, width=30,
+                                  font=("proxima-nova", 13), show="*")
+        password_entry.place(x=100, y=30)
+
+        def reset(username_entry, password_entry):
+            self.tb_text.set("") 
+            username_entry.delete(0, tk.END)
+            password_entry.delete(0, tk.END)
+
+        # Xử lí dữ liệu đăng nhập
+
+        def login():
+            tb_frame = tk.Frame(self, bg="#f9f4ef", height=30, width=350)
+            tb_label = tk.Label(tb_frame, textvariable=self.tb_text,font=("proxima-nova", 12), fg="red", bg="#f9f4ef")
+            tb_label.pack(side=LEFT)
+            tb_frame.place(x=470, y=300)
+            username = username_entry.get()
+            password = password_entry.get()
+            if not username:
+                self.tb_text.set("Bạn chưa nhập tài khoản.")
+                return
+            if username and not password:
+                self.tb_text.set("Bạn chưa nhập mật khẩu.")
+                return
+            if username in list_user.ds:
+                if list_user.ds[username] == password:
+                    self.tb_text.set("")
+                    from .MH_BanHang import MH_BanHang
+                    controller.show_frame(MH_BanHang)
+                    reset(username_entry, password_entry)
+                else:
+                    self.tb_text.set("Sai mật khẩu.")
+            else:
+                self.tb_text.set("Sai tài khoản hoặc mật khẩu. Hãy kiểm tra lại.")
+
+
+
+        # ===================== HÌNH ẢNH =====================
+        path_img = "images/name_cafe.png"
+        img = Image.open(path_img)
+        img = img.resize((700, 200), Image.Resampling.LANCZOS)
+        name_img = ImageTk.PhotoImage(img)
+        label_img = tk.Label(self, image=name_img, bd=0)
+        label_img.image = name_img
+        label_img.place(x=290, y=100)
+
+        # ===================== NÚT ĐĂNG NHẬP =====================
+        tk.Button(
+            self, text="Đăng nhập",
+            bg="#fffffe", fg="#e45858",
+            font=("proxima-nova", 10, "bold"),
+            command=login
+        ).place(x=570, y=460, width=90, height=25)
+
+        # ===================== NÚT THOÁT =====================
+        tk.Button(
+            self, text="Thoát",
+            bg="#fffffe", fg="#e45858",
+            font=("proxima-nova", 10, "bold"),
+            command=controller.destroy
+        ).place(x=680, y=460, width=90, height=25)
