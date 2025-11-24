03691e1b537de6c6343093e2341fad79849edfca
Nguyên Trần <1trannguyen10@gmail.com>
2025-11-24 08:53:24 +0700

    thay đổi lại tránh lỗi tab với space

--- DoAnPython/objects/Ban.py
+++ DoAnPython/objects/Ban.py
@@ -7,7 +7,13 @@ class Ban:
         self.ten_ban = ten_ban
         self.trang_thai = trang_thai
         self.thoi_gian = thoi_gian
-        self.ds_mon = ds_mon        
+        # Khi đọc file, ds_mon có thể chỉ là danh sách dict,
+        # cần đảm bảo chuyển đổi nó thành đối tượng Mon nếu cần thiết
+        if all(isinstance(m, dict) for m in ds_mon):
+            self.ds_mon = [Mon(m['ma_mon'], m['ten_mon'], m['don_gia'], m['so_luong']) for m in ds_mon]
+        else:
+            self.ds_mon = ds_mon
+
         self.giam_gia = giam_gia
 
     def to_dict(self):
@@ -32,39 +38,46 @@ class DanhSachBan:
         self.ds = []
 
     def doc_file(self, filename):
-    	try:
-    		with open(filename, "r", encoding = "utf-8") as f:
-    			du_lieu = json.load(f)
-    			self.ds = [Ban(i["ma_ban"], i["ten_ban"], i["trang_thai"],
-    				i["thoi_gian"], i["ds_mon"], i["giam_gia"]) for i in du_lieu]
-    	except Exception as loi:
-    		print("Lỗi đọc file: ",loi)
+        try:
+            with open(filename, "r", encoding="utf-8") as f:
+                du_lieu = json.load(f)
+                # Dữ liệu đọc từ JSON là list of dicts, cần chuyển về list of Ban objects
+                self.ds = [Ban(
+                    i["ma_ban"],
+                    i["ten_ban"],
+                    i["trang_thai"],
+                    i["thoi_gian"],
+                    i["ds_mon"], # Danh sách món ở đây đang là list of dicts, class Ban sẽ xử lý
+                    i["giam_gia"]
+                ) for i in du_lieu]
+        except Exception as loi:
+            print("Lỗi đọc file: ", loi)
 
     def ghi_file(self, filename):
-    	try:
-    		with open(filename, "w", encoding = "utf-8") as f:
-    			json.dump([i.to_dict() for i in self.ds], f, ensure_ascii = False, indent = 4)
-    	except Exception as loi:
-    		print("Lỗi ghi file: ",loi)
+        try:
+            with open(filename, "w", encoding="utf-8") as f:
+                json.dump([i.to_dict() for i in self.ds], f, ensure_ascii=False, indent=4)
+        except Exception as loi:
+            print("Lỗi ghi file: ", loi)
 
 
     def free(self):
-    	cnt = 0
-    	for i in self.ds:
-    		if i.trang_thai == "Free":
-    			cnt += 1
-    	return cnt
+        cnt = 0
+        for i in self.ds:
+            if i.trang_thai == "Free":
+                cnt += 1
+        return cnt
 
     def serve(self):
-    	cnt = 0
-    	for i in self.ds:
-    		if i.trang_thai == "Serve":
-    			cnt += 1
-    	return cnt
+        cnt = 0
+        for i in self.ds:
+            if i.trang_thai == "Serve":
+                cnt += 1
+        return cnt
 
     def booked(self):
-    	cnt = 0
-    	for i in self.ds:
-    		if i.trang_thai == "Booked":
-    			cnt += 1
-    	return cnt
+        cnt = 0
+        for i in self.ds:
+            if i.trang_thai == "Booked":
+                cnt += 1
+        return cnt
